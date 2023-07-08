cd0210cb9702a628e5909908ee09808d4f538798
DenShykyta <wrkacc4@gmail.com>
2023-07-08 23:12:43 +0300

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,7 +1,6 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
 const { nanoid } = require("nanoid");
-const { write } = require("node:fs");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
